ed404b1f476ce152a4e0eb6dd757dcad83bf9cbc
Ivan Melnichuk <cadet.ua@xakep.ru>
2014-12-02 12:11:22 +0200

    ant build files for extensions. Properties files for Eclipse. fixed .gitignore

---
+++ dde/src/ua/com/integer/dde/Version.java
@@ -0,0 +1,13 @@
+package ua.com.integer.dde;
+
+/**
+ * DDE version
+ * 
+ * @author 1nt3g3r
+ */
+public class Version {
+	/**
+	 * Current version
+	 */
+	public static final float NUMBER = 0.01f;
+}
